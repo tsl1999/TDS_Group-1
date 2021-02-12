f50f9cfaa8bc64b1b3313b22511f8eb57d6d7902
Tianshu Liu <tl1020@login-9.ib0.cx2.hpc.ic.ac.uk>
2021-02-12 10:53:50 +0000

    some new changes regarding 30  biomarkers

--- categorise_code.R
+++ categorise_code.R
@@ -4,7 +4,7 @@
 #read data----------------------------------------------
 path="/rds/general/project/hda_students_data/live/Group1"
 setwd(path)
-merged<- readRDS("/rds/general/project/hda_students_data/live/Group1/merged.rds")
+
 data_dictonary <- read_excel("data_dictonary.xlsx")
 data_cat<-data.frame(data_dictonary[,c("Field ID","code ID")])
 disease_outcomes <- readRDS("/rds/general/project/hda_students_data/live/Group1/disease_outcomes.rds")
@@ -16,7 +16,8 @@ mycoding=data.frame(read.csv("Codings_Showcase.csv"))
 
 
 #categorise function--------------------------------------
-  
+dataframe<-readRDS("/rds/general/project/hda_students_data/live/Group1/data/merged_no_withdraw.rds")
+
   for(i in 1: length(colnames(dataframe))){
       if (is.na(data_cat[sub("\\..*", "", colnames(dataframe)[i]),2])==F) 
       {
@@ -32,10 +33,12 @@ mycoding=data.frame(read.csv("Codings_Showcase.csv"))
       
   }
 
-dataframe<-merged_no_withdraw
+#test with i=61,which the code =87, you can see the problem with chapter V
+i=87
+coding_id=data_cat[sub("\\..*", "", colnames(dataframe)[i]),2]
+mycoding=read.csv("Codings_Showcase.csv")
+mycoding_field=mycoding[which(mycoding[,1]==coding_id),]
+mycoding_field=mycoding_field[,-1]
+rownames(mycoding_field)=mycoding_field$Value
+dataframe[,i]<-as.character(mycoding_field[as.character(dataframe[,i]),"Meaning"])
 
-
-#test---------------------------------------------------------------------
-i=130
-
-table(merged[,i])
