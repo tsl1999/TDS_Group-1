480d7c24ac55c859ed0881dffeffd13d3db874a7
Tianshu Liu <tl1020@login-9.ib0.cx2.hpc.ic.ac.uk>
2021-02-12 10:53:50 +0000

    some new changes regarding 30  biomarkers

--- merged.R
+++ merged.R
@@ -3,9 +3,10 @@ library(data.table)
 
 path="/rds/general/project/hda_students_data/live/Group1"
 setwd(path)
-ukb27725=data.frame(fread("/rds/general/project/hda_students_data/live/Group1/ukb27725.csv"))
+biomarkers<-readRDS("/rds/general/project/hda_students_data/live/Group1/data/biomarkers.rds")
 individual_cov<-readRDS("/rds/general/project/hda_students_data/live/Group1/individual_covariates.rds")
 
-merged<-merge(individual_cov,ukb27725,by="eid",no.dups = FALSE)
+merged<-merge(individual_cov,biomarkers,by="eid",no.dups = FALSE)
 
-isTRUE(merged$X40001.0.0.x==merged$X40001.0.0.y)
+saveRDS(merged,"merged.rds")
+
